b24f89f0807776e74554d52f52fad01c98826e38
9mean3 <98880041+9mean3@users.noreply.github.com>
2023-03-20 23:55:51 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<time.h>
+using namespace std;
+
+int main(){
+int t, r;
+	string s;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		cin >> r;
+		cin >> s;
+		for (int j = 0; j < s.size(); j++) {
+			for (int k = 0; k < r; k++) {
+				cout << s[j];
+			}
+            
+		}
+        cout << endl;
+	}}
