0aaa83e986afa41109f36992e5bb14db08ed4fab
9mean3 <98880041+9mean3@users.noreply.github.com>
2023-07-13 16:24:32 +0900

    [Gold IV] Title: PPAP, Time: 36 ms, Memory: 5092 KB -BaekjoonHub

---
+++ 백준/Gold/16120. PPAP/PPAP.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    	ios::sync_with_stdio(false);
+	string str, bomb = "PPAP", answer, check;
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
+	{
+		answer += str[i];
+		if (str[i] == bomb.back() && answer.size() >= bomb.size())
+		{
+			check = "";
+			for (int j = 0; j < bomb.size(); j++) {
+				check += answer[(answer.size() - bomb.size()) + j];
+			}
+			//cout << check << endl;
+			if (check == bomb) {
+				answer.insert(answer.end() - bomb.size(), 'P');
+				answer.erase(answer.end() - bomb.size(), answer.end());
+				//str.erase(i - (bomb.size()), i);
+				//cout << "지움" << endl;
+			}
+		}
+	}
+	if (answer == "P")
+		cout << "PPAP";
+	else
+		cout << "NP";
+}
