24ec5e8f6a193848022f0a217b673dabf926a7e0
9mean3 <98880041+9mean3@users.noreply.github.com>
2023-08-14 06:01:24 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<stack>
+#include<deque>
+#include<sstream>
+using namespace std;
+
+int main() {
+	cout << "     /~\\\n";
+	cout << "    ( oo|\n";
+	cout << "    _\\=/_\n";
+	cout << "   /  _  \\\n";
+	cout << "  //|/.\\|\\\\\n";
+	cout << " ||  \\ /  ||\n";
+	cout << "============\n";
+	cout << "|          |\n";
+	cout << "|          |\n";
+	cout << "|          |\n";
+}
