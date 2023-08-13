b830c9728ab5a9fa3699182a1c909a5394fafc5d
9mean3 <98880041+9mean3@users.noreply.github.com>
2023-08-14 06:02:57 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<stack>
+#include<deque>
+#include<sstream>
+using namespace std;
+
+int main() {
+		int n = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		int input;
+		cin >> input;
+		n += input;
+	}
+	cout << n;
+}
