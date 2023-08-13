698414e94c48c64a842b20a5e7c59f854d8b0db6
9mean3 <98880041+9mean3@users.noreply.github.com>
2023-08-14 05:58:26 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<stack>
+#include<deque>
+#include<sstream>
+using namespace std;
+
+int main() {
+	cout << ".  .   .\n";
+	cout << "|  | _ | _. _ ._ _  _\n";
+	cout << "|/\\|(/.|(_.(_)[ | )(/.\n";
+}
