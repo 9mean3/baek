5f54a18f3cc9c5cf92c1a70b3e632189f1550504
9mean3 <98880041+9mean3@users.noreply.github.com>
2023-08-14 06:24:05 +0900

    [Bronze V] Title: Can you add this?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<stack>
+#include<deque>
+#include<sstream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		long long a, b;
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+}
