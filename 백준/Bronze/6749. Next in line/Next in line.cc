ce127aac4cb39ffaaba1ddb32ce6dd76164b1521
9mean3 <98880041+9mean3@users.noreply.github.com>
2023-08-14 06:05:12 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<stack>
+#include<deque>
+#include<sstream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << b + (b - a);
+}
