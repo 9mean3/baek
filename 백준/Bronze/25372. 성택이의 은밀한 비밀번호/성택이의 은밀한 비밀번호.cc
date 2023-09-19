588535589ecb2410bf5ad46639ffbc1742deac76
9mean3 <98880041+9mean3@users.noreply.github.com>
2023-09-20 00:06:07 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        string s;
+        cin >> s;
+        
+        if(s.size() >= 6 && s.size() <= 9)
+            cout << "yes" << endl;
+        else
+            cout << "no" << endl;
+    }
+}
