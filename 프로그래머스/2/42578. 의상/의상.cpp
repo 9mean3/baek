0314bcca1527dacb38774646da1dd755f5a77aee
9mean3 <98880041+9mean3@users.noreply.github.com>
2023-10-23 12:13:29 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -7,16 +7,13 @@ using namespace std;
 int solution(vector<vector<string>> clothes) {
 	int answer = 1;
 	map<string, int> cat;
-	int catsize = 0;
 	for (int i = 0; i < clothes.size(); i++)
 	{
-		if (cat[clothes[i][1]] == 0) catsize++;
 		cat[clothes[i][1]]++;
 	}
 	for (auto i : cat)
 	{
 		answer *= (i.second + 1);
 	}
-	answer -= 1;
-	return answer;
+	return --answer;
 }
