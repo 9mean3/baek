c9db02f206b37a9a50473e88106b3095ed6f01d3
9mean3 <98880041+9mean3@users.noreply.github.com>
2023-10-31 17:46:17 +0900

    [Gold V] Title: ABCDE, Time: 32 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.cc
+++ 백준/Gold/13023. ABCDE/ABCDE.cc
@@ -4,11 +4,13 @@ using namespace std;
 
 int n, m;
 vector<int> board[2001];
-bool used[2001] = {0,};
+bool used[2001] = { 0, };
 bool answer = false;
 
 void func(int n, int length) {
-	if (length >= 4) {answer = true; return;}
+	if (length >= 4) {
+		answer = true; return;
+	}
 	used[n] = true;
 	length++;
 	for (auto i : board[n]) {
