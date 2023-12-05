a1d373dcf6ae10984f1ea09c630c3963ca797cee
9mean3 <98880041+9mean3@users.noreply.github.com>
2023-12-05 15:34:05 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long dp[1000001] = { 0, 1, 2 };
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int input;
+	cin >> input;
+	for (int i = 3; i <= input; i++)
+	{
+		dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+
+	}
+	cout << dp[input];
+}
