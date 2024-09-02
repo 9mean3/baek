8d7340405691b0d63cd834ca35d8e0754a32c23c
9mean3 <98880041+9mean3@users.noreply.github.com>
2024-09-02 18:20:52 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float s1, s2;
+    cin >> s1 >> s2;
+    if(s1 >= s2 / 2){
+        cout << "E" << endl;
+    }
+    else{
+        cout << "H" << endl;
+    }
+}
